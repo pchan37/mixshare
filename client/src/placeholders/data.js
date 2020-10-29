7bccd34f2bb38350abe575f3cf64b286c06a7edf
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-10-29 06:43:52 +0000

    Prototyped the friends page (#6)

---
+++ client/src/placeholders/data.js
@@ -0,0 +1,42 @@
+const data = {
+  friends: [
+    {
+      id: 1,
+      username: 'Jo',
+    },
+    {
+      id: 2,
+      username: 'Jasmine',
+    },
+    {
+      id: 3,
+      username: 'Wayne',
+    },
+    {
+      id: 4,
+      username: 'Daniel',
+    },
+    {
+      id: 5,
+      username: 'Clifford',
+    },
+    {
+      id: 6,
+      username: 'Elmo',
+    },
+    {
+      id: 7,
+      username: 'Barney',
+    },
+    {
+      id: 8,
+      username: 'CookieMonster',
+    },
+    {
+      id: 9,
+      username: 'PowerRanger',
+    },
+  ],
+};
+
+export default data;
