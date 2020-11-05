6c664ad19ecac6a4dbc4c532a9292cf81728595b
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-05 04:12:28 -0500

    Run prettier on server js files

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,19 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // retrieve the currently logged in user
-router.get('/user', (req, res) => {
-});
+router.get('/user', (req, res) => {});
 
 // attempt to log the client in
-router.post('/login', async (req, res, next) => {
-});
+router.post('/login', async (req, res, next) => {});
 
 // attempt to register the client
-router.post('/register', async (req, res, next) => {
-});
+router.post('/register', async (req, res, next) => {});
 
 // delete the client's session
-router.post('/logout', async (req, res, next) => {
-});
+router.post('/logout', async (req, res, next) => {});
 
 module.exports = router;
