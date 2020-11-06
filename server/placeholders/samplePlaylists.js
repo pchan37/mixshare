0b839d6a3b2ebec0b3bb71f7fad17c5636eee396
jogao01 <joyce.gao01@gmail.com>
2020-11-06 11:24:03 -0500

    added more sample playlist data

--- server/placeholders/samplePlaylists.js
+++ server/placeholders/samplePlaylists.js
@@ -5,8 +5,8 @@ const samplePlaylists = {
       owner: 'Jo',
       name: 'Eat the Depression',
     },
-    { id: 2, owner: 2, name: 'Apart' },
-    { id: 3, owner: 3, name: 'Pop Rising' },
+    { id: 2, owner: 'Clifford', name: 'Apart' },
+    { id: 3, owner: 'Wayne', name: 'Pop Rising' },
     {
       id: 4,
       owner: 'Jasmine',
@@ -22,6 +22,14 @@ const samplePlaylists = {
       owner: 'Her',
       name: 'Let me Go',
     },
+    { id: 7, owner: 'Clifford', name: 'Lofi Beats' },
+    { id: 8, owner: 'Wayne', name: 'Zany Rock' },
+    { id: 9, owner: 'Patrick', name: 'Musical Soundtracks' },
+    { id: 10, owner: 'Jo', name: 'Kimbops' },
+    { id: 11, owner: 'Jasmine', name: 'Musical Soundtracks' },
+    { id: 12, owner: 'Jo', name: 'Canto Bops' },
+    { id: 13, owner: 'Clifford', name: 'Are and Be' },
+    { id: 14, owner: 'Jo', name: 'Why do it be like this' },
   ],
 };
 
