fe737538d80fd9054855807d94984f15ff48c74d
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-05 23:05:04 -0500

    Set up mongoose connection and configure passport

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const MongoStore = require('connect-mongo')(session);
 
+const connection = require('./config/database');
 const indexRouter = require('./routes/index');
 
 require('dotenv').config();
@@ -12,6 +16,34 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.SECRET));
 app.use(express.urlencoded({ extended: true }));
+
+const sessionConfig = {
+  name: 'sid',
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: connection,
+    collection: 'sessions',
+  }),
+  rolling: true,
+  cookie: {
+    maxAge: 1000 * 60 * 15,
+    secure: false,
+    sameSite: 'strict',
+  },
+};
+
+if (app.get('env') == 'production') {
+  app.set('trust proxy', 1);
+  sessionConfig.cookie.secure = true;
+}
+
+app.use(session(sessionConfig));
+
+require('./config/passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 
---
+++ server/config/passport.js
@@ -0,0 +1,60 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const customFields = {
+  usernameField: 'username',
+  passwordField: 'password',
+};
+
+const verifyCallback = (username, password, done) => {
+  console.log('Verifying auth callback.');
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+      } else {
+        return done(null, false);
+      }
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (err) {
+          return done(err);
+        }
+        if (res) {
+          console.log('Found a user whose username and password matches.');
+          return done(null, user);
+        }
+        console.log('No user found with the password.');
+        return done(null, false);
+      });
+      return done(null, false);
+    })
+    .catch((err) => {
+      console.log(
+        `Encountered "${err.message}" while verifying auth callback.`
+      );
+      done(err);
+    });
+};
+
+const strategy = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  user.password = bcrypt.hashSync(user.password, 10);
+
+  console.log(`Serializing user... ${JSON.stringify(user)}`);
+  done(null, user._id);
+});
+
+passport.deserializeUser((userId, done) => {
+  User.findById(userId)
+    .then((user) => {
+      console.log(`Deserialized user... ${JSON.stringify(user)}`);
+      done(null, user);
+    })
+    .catch((err) => {
+      console.log(`Encountered "${err.message}" while deserializing user.`);
+      done(err);
+    });
+});
