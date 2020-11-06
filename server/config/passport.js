fd4d2cbe6ac868296138e458cb0fe69f8799ffc8
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-06 00:49:38 -0500

    Add User model and implement the GET /user route

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const { User } = require('../database/models');
 const bcrypt = require('bcrypt');
 
 const customFields = {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+const { User } = require('../database/models');
 
 // retrieve the currently logged in user
-router.get('/user', (req, res) => {});
+router.get('/user', (req, res) => {
+  if (req.user !== undefined) {
+    return res.json({ username: req.user.username });
+  }
+
+  console.log('No existing session found.');
+  return res.json({});
+});
 
 // attempt to log the client in
 router.post('/login', async (req, res, next) => {});
