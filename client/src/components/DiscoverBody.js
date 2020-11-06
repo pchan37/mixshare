a7a18f83681ee80c5585ce83db5bd5bbaed60cf3
jogao01 <joyce.gao01@gmail.com>
2020-11-05 23:12:48 -0500

    started working on search for youtube api

--- client/src/components/DiscoverBody.js
+++ client/src/components/DiscoverBody.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 import { Button, Container, Form } from 'react-bootstrap';
 
-import { DisplayThumbnails } from './';
+import { Thumbnail } from './';
+import data from '../placeholders/data';
 
-const DiscoverBody = () => {
+import SearchIcon from '@material-ui/icons/Search';
+
+function DiscoverBody() {
+  const [playlists, setPlaylists] = useState([]);
+
+  const getSearch = async (event) => {
+    event.preventDefault();
+    const query = event.target.elements.query.value;
+    console.log(query);
+    try {
+      const res = await Axios.post('../api/youtube/songs', {
+        query: query,
+      });
+      setPlaylists(res.data.playlists);
+      console.log(playlists);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  //const DiscoverBody = () => {
   return (
     <div className="d-flex flex-column">
       <div className="d-flex flex-row mb-2">
         <h2>Discover</h2>
         <Container fluid className="d-flex justify-content-end">
-          <Form className="d-flex flex-row">
-            <Form.Control placeholder="Search" />
-            <span>
-              <Button>Search</Button>
-            </span>
+          <Form className="d-flex flex-row" onSubmit={getSearch}>
+            <Form.Control name="query" type="text" placeholder="Search" />
+
+            <Button type="submit" variant="flat">
+              <SearchIcon style={{ color: '#979696' }} />
+            </Button>
           </Form>
         </Container>
       </div>
@@ -22,16 +45,27 @@ const DiscoverBody = () => {
       <div className="d-flex flex-column">
         <h5>Top Songs</h5>
         <div className="d-flex flex-row">
-          <DisplayThumbnails />
+          {data.songs.map((p) => {
+            return (
+              <Thumbnail key={p.id} name={p.name} artist={p.artist}></Thumbnail>
+            );
+          })}
         </div>
       </div>
 
       <div className="d-flex flex-column">
         <h5>Top Playlists</h5>
-        <DisplayThumbnails />
+        <div className="d-flex flex-row">
+          {data.playlists.map((p) => {
+            return (
+              <Thumbnail key={p.id} name={p.name} artist={p.owner}></Thumbnail>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
-};
+  //};
+}
 
 export default DiscoverBody;
--- server/routes/youtube.js
+++ server/routes/youtube.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const apiKey = AIzaSyCqRkLe3nqTjE7yHIeqMn6jprdkEQPTec8;
+
+//search youtube API
+router.post('/songs', async (req, res) => {
+  console.log('Hey');
+  /* res = YouTube.Search.list('id,snippet', {
+    q: req,
+    maxResults: 10,
+    key: apiKey,
+  });*/
+  return ['Hello'];
+});
 
 module.exports = router;
