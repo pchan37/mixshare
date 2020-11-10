da192b615b98d2ffb7d50f818bcc5db0bf9527f4
jasmine <jasminechin12>
2020-11-09 21:52:13 -0500

    fixed small typo and added curly brackets around Edit in imports

--- client/src/components/AccountDetails.js
+++ client/src/components/AccountDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Container, Row, Col, Button, OverlayTrigger } from 'react-bootstrap';
-import Edit from '@material-ui/iconst';
+import { Edit } from '@material-ui/icons';
 import { ChangeUsernamePopup, ChangePasswordPopup } from './';
 
 const AccountDetails = () => {
