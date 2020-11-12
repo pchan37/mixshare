829881913048c2389e773cd6504369b6482f32be
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-12 05:00:49 +0000

    displays username on account management page (#34)
    Co-authored-by: jogao01 <joyce.gao01@gmail.com>

--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { AccountCircle } from '@material-ui/icons';
 
-const ProfileDetails = () => {
+const ProfileDetails = (props) => {
   return (
     <Container>
       <div className="d-flex flex-row" style={{ alignItems: 'center' }}>
         <AccountCircle style={{ color: '#979696', fontSize: 200 }} />
-        <h2>Username's Profile</h2>
+        <h2>{props.username}</h2>
       </div>
     </Container>
   );
