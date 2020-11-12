a91b5ddba274ed1696f529ab6592e3228daa99fd
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-12 00:41:50 -0500

    Retrieve the username on each page load and allow easy access to retrieved username

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -5,8 +6,10 @@ import {
   Redirect,
 } from 'react-router-dom';
 
+import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { UserContext } from './contexts';
 import {
   AccountSettings,
   Discover,
@@ -21,22 +24,39 @@ import {
 } from './pages';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const res = await Axios.get('/api/auth/user');
+        setCurrentUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchUsername();
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/account" component={AccountSettings} />
-        <Route exact path="/discover" component={Discover} />
-        <Route exact path="/playlists" component={MyPlaylists} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/searchResults" component={SearchResults} />
-        <Route exact path="/edit" component={PlaylistEditor} />
-        <Route exact path="/friends" component={Friends} />
-        <Route exact path="/gifts" component={Gifts} />
-        <Route path="/404" component={NotFound} />
-        <Redirect to="/404" />
-      </Switch>
-    </Router>
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/account" component={AccountSettings} />
+          <Route exact path="/discover" component={Discover} />
+          <Route exact path="/playlists" component={MyPlaylists} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/searchResults" component={SearchResults} />
+          <Route exact path="/edit" component={PlaylistEditor} />
+          <Route exact path="/friends" component={Friends} />
+          <Route exact path="/gifts" component={Gifts} />
+          <Route path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
