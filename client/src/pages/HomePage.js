c9d46fcd5eb075365a44309f5b5d319b22d81ad3
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-11-17 23:35:21 -0500

    Make the homepage more responsive

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,25 +1,21 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
-import { Container, Figure, Row, Col } from 'react-bootstrap';
+import Logo from '../assets/images/logo.png';
 import { SignUpLogin } from '../components';
 import { BasicLayout } from '../layout';
-import Logo from '../assets/images/logo.png';
 
 const HomePage = () => {
   return (
     <BasicLayout>
-      <div style={{ height: '100vh', padding: '30vh 0 0 0' }}>
-        <Container>
-          <Row>
-            <Col class="my-auto">
-              <Figure.Image width={550} height={550} src={Logo} />
-            </Col>
-            <Col xs={1}></Col>
-            <Col>
-              <SignUpLogin />
-            </Col>
-          </Row>
-        </Container>
+      <div className="d-flex align-items-center h-100 justify-content-center">
+        <div className="d-flex align-items-center h-100 p-5 w-50">
+          <Image src={Logo} fluid />
+        </div>
+
+        <div className="d-flex flex-column align-items-center h-100 justify-content-center w-25">
+          <SignUpLogin />
+        </div>
       </div>
     </BasicLayout>
   );
