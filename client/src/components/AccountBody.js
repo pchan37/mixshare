b5937a9cb8c059c503f5bb37a80b0f5c3ffb00f6
jogao01 <joyce.gao01@gmail.com>
2020-12-08 17:36:52 -0500

    removed change profile picture button

--- client/src/components/AccountBody.js
+++ client/src/components/AccountBody.js
@@ -16,18 +16,6 @@ import { AccountDetails, DeletePopup } from './';
 
 import { UserContext } from '../contexts';
 
-const ChangeProfilePicPopup = (
-  <Popover id="popover-basic">
-    <Popover.Content>
-      <Form>
-        <Form.Group class="p-1">
-          <Form.File id="profile-pic" label="Select a Profile Picture" />
-        </Form.Group>
-      </Form>
-    </Popover.Content>
-  </Popover>
-);
-
 const AccountBody = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
@@ -66,15 +54,6 @@ const AccountBody = () => {
         <Col>
           <AccountDetails />
         </Col>
-      </Row>
-      <Row style={{ paddingLeft: 20, paddingTop: 15 }}>
-        <OverlayTrigger
-          placement="bottom"
-          delay={{ show: 250, hide: 400 }}
-          overlay={ChangeProfilePicPopup}
-          trigger="click">
-          <Button variant="light">Change Profile Picture</Button>
-        </OverlayTrigger>
       </Row>
       <Row style={{ paddingLeft: 30, paddingTop: 200 }}>
         <DeletePopup
